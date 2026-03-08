466b85c3199cd32b941f29d784aad16d107e4587
AnochiwaAlfred <anochiwaalfred@gmail.com>
2026-03-08 16:27:31 +0100

    first commit

--- accounts/models.py
+++ accounts/models.py
@@ -6,6 +6,6 @@ from django.contrib.auth.models import User
 
 class SpotifyToken(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    access_token = models.CharField(max_length=255)
-    refresh_token = models.CharField(max_length=255)
+    access_token = models.TextField()
+    refresh_token = models.TextField()
     expires_at = models.DateTimeField()
